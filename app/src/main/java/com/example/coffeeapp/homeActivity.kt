cf5b3db52d744574e95c653981646e63715c6c43
Bassant Samer <61835135+bassantsamerr@users.noreply.github.com>
2022-08-15 12:03:29 +0200

    adding drawable files

--- app/src/main/java/com/example/coffeeapp/homeActivity.kt
+++ app/src/main/java/com/example/coffeeapp/homeActivity.kt
@@ -2,12 +2,13 @@ package com.example.coffeeapp
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class homeActivity : AppCompatActivity() {
-
+data class drink(val Name:String,val Price:Double,val imageView: ImageView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -19,4 +20,5 @@ class homeActivity : AppCompatActivity() {
             Toast.makeText(this,"Item Selected " +drinks[i],Toast.LENGTH_LONG).show()
         }
 
-}}
+}
+}
