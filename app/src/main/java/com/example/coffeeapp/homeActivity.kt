a25152352aec54c6d68b6911166a6921943d3733
Bassant Samer <61835135+bassantsamerr@users.noreply.github.com>
2022-08-16 13:58:13 +0200

    done preference logic

--- app/src/main/java/com/example/coffeeapp/homeActivity.kt
+++ app/src/main/java/com/example/coffeeapp/homeActivity.kt
@@ -2,27 +2,54 @@ package com.example.coffeeapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ImageView
 import android.widget.ListView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+
+import java.util.ArrayList
 
 class homeActivity : AppCompatActivity() {
-data class drink(val Name:String,val Price:Double,val imageView: ImageView)
+
+    private var lv: ListView? = null
+    private var customeAdapter: menuAdapter? = null
+    private var imageModelArrayList: ArrayList<ImageModel>? = null
+    private val myImageList = intArrayOf(R.drawable.cappuccino, R.drawable.espresso, R.drawable.latte, R.drawable.macchiato, R.drawable.mocha)
+    private val myImageNameList = arrayOf("Cappuccino", "Espresso", "Latte", "Macchiato", "Mocha")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val listView=findViewById<ListView>(R.id.listview)
-        val drinks = arrayOf("Espresso","Cappuccino","Macchiato","Mocha","Latte")
-        val arrayAdapter:ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1,drinks)
-        listView.adapter=arrayAdapter
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this,"Item Selected " +drinks[i],Toast.LENGTH_LONG).show()
+
+        lv = findViewById(R.id.listView) as ListView
+
+        imageModelArrayList = populateList()
+        Log.d("1", imageModelArrayList!!.size.toString() + "")
+        customeAdapter = menuAdapter(this, imageModelArrayList!!)
+        lv!!.adapter = customeAdapter
+        lv!!.setOnItemClickListener { adapterView, view, i, l ->
+          //  Toast.makeText(this,"Item Selected " +lv!!.findViewById<TextView>(R.id.name).text.toString(),Toast.LENGTH_LONG).show()
             startPreference()
         }
 
-}
+
+    }
+
+    private fun populateList(): ArrayList<ImageModel> {
+
+        val list = ArrayList<ImageModel>()
+
+        for (i in 0..4) {
+            val imageModel = ImageModel()
+            imageModel.setNames(myImageNameList[i])
+            imageModel.setImage_drawables(myImageList[i])
+            list.add(imageModel)
+        }
+
+        return list
+    }
+
     fun startPreference(){
         val intent = Intent(this,preferenceActivity::class.java)
         startActivity(intent)
--- app/src/main/java/com/example/coffeeapp/preferenceActivity.kt
+++ app/src/main/java/com/example/coffeeapp/preferenceActivity.kt
@@ -1,14 +1,132 @@
 package com.example.coffeeapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+var total:TextView? = null
+var quantity = 0
 class preferenceActivity : AppCompatActivity() {
+    var imageView: ImageView? = null
+    var plusquantity: ImageButton? = null
+    var minusquantity:ImageButton? = null
+    var quantitynumber: TextView? = null
+    var drinkName:TextView? = null
+    //var total:TextView? = null
+    var coffeePrice:TextView?=null
+    var addtoCart: Button? = null
+    var mediumcup:ImageButton?=null
+    var smallcup:ImageButton?=null
+    var largecup:ImageButton?=null
+    var zerosugarcube:ImageButton?=null
+    var onesugarcube:ImageButton?=null
+    var twosugarcube:ImageButton?=null
+    var threesugarcube:ImageButton?=null
+    //var quantity = 0
+    var hasAllRequiredValues = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
+
+        imageView = findViewById(R.id.imageViewInfo)
+        plusquantity = findViewById(R.id.addquantity)
+        minusquantity  = findViewById(R.id.subquantity)
+        quantitynumber = findViewById(R.id.quantity)
+        drinkName = findViewById(R.id.drinkNameinInfo)
+        total = findViewById(R.id.total)
+        addtoCart = findViewById(R.id.btn_addToCart)
+        coffeePrice=findViewById(R.id.coffeePrice)
+        mediumcup=findViewById(R.id.medium_cup)
+        smallcup=findViewById(R.id.small_cup)
+        largecup=findViewById(R.id.large_cup)
+        zerosugarcube=findViewById(R.id.zerosugarcube)
+        onesugarcube=findViewById(R.id.onesugarcube)
+        twosugarcube=findViewById(R.id.twosugarcube)
+        threesugarcube=findViewById(R.id.threesugarcube)
+
+
+        Log.v("1", "before button add")
+        plusquantity?.setOnClickListener {
+            Log.v("2", "inside button add")
+                // coffee price
+                var basePrice = coffeePrice?.text.toString().toInt()
+                quantity++
+                displayQuantity()
+                val Total = basePrice + total?.text.toString().toInt()
+                val setnewPrice = Total.toString()
+                total?.setText(setnewPrice)
+
+        }
+
+        minusquantity?.setOnClickListener(View.OnClickListener {
+            val basePrice = coffeePrice?.text.toString().toInt()
+            // because we dont want the quantity go less than 0
+            if (quantity == 0) {
+                Toast.makeText(this, "Cant decrease quantity < 0", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                quantity--
+                displayQuantity()
+                val coffePrice =total?.text.toString().toInt()- basePrice
+                val setnewPrice = coffePrice.toString()
+                total?.setText(setnewPrice)
+
+            }
+        })
+
+        mediumcup?.setOnClickListener({
+            mediumcup?.setBackgroundResource(R.drawable.coffeecupselected)
+            smallcup?.setBackgroundResource(R.drawable.coffeecup)
+            largecup?.setBackgroundResource(R.drawable.coffeecup)
+            coffeePrice?.setText("40")
+        })
+        largecup?.setOnClickListener({
+            largecup?.setBackgroundResource(R.drawable.coffeecupselected)
+            smallcup?.setBackgroundResource(R.drawable.coffeecup)
+            mediumcup?.setBackgroundResource(R.drawable.coffeecup)
+            coffeePrice?.setText("50")
+        })
+        smallcup?.setOnClickListener({
+            smallcup?.setBackgroundResource(R.drawable.coffeecupselected)
+            mediumcup?.setBackgroundResource(R.drawable.coffeecup)
+            largecup?.setBackgroundResource(R.drawable.coffeecup)
+            coffeePrice?.setText("30")
+        })
+
+        zerosugarcube?.setOnClickListener({
+            zerosugarcube?.setBackgroundResource(R.drawable.zerosugarcubeselected)
+            onesugarcube?.setBackgroundResource(R.drawable.onesugarcube)
+            twosugarcube?.setBackgroundResource(R.drawable.twosugarcubes)
+            threesugarcube?.setBackgroundResource(R.drawable.threesugarcubes)
+
+        })
+
+        onesugarcube?.setOnClickListener({
+            zerosugarcube?.setBackgroundResource(R.drawable.zerosugarcube)
+            onesugarcube?.setBackgroundResource(R.drawable.onesugarcubeselected)
+            twosugarcube?.setBackgroundResource(R.drawable.twosugarcubes)
+            threesugarcube?.setBackgroundResource(R.drawable.threesugarcubes)
+
+        })
+
+        twosugarcube?.setOnClickListener({
+            zerosugarcube?.setBackgroundResource(R.drawable.zerosugarcube)
+            onesugarcube?.setBackgroundResource(R.drawable.onesugarcube)
+            twosugarcube?.setBackgroundResource(R.drawable.twosugarcubeselected)
+            threesugarcube?.setBackgroundResource(R.drawable.threesugarcubes)
+
+        })
+
+        threesugarcube?.setOnClickListener({
+            zerosugarcube?.setBackgroundResource(R.drawable.zerosugarcube)
+            onesugarcube?.setBackgroundResource(R.drawable.onesugarcube)
+            twosugarcube?.setBackgroundResource(R.drawable.twosugarcubes)
+            threesugarcube?.setBackgroundResource(R.drawable.threesugarcubeselected)
+
+        })
+
         val btnAddToCard = findViewById<Button>(R.id.btn_addToCart)
         btnAddToCard.setOnClickListener{
             startHome()
@@ -18,4 +136,8 @@ class preferenceActivity : AppCompatActivity() {
         val intent =Intent(this,homeActivity::class.java)
         startActivity(intent)
     }
+    private fun displayQuantity() {
+        quantitynumber!!.text = quantity.toString()
+    }
+
 }
